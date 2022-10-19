f7f8e6a2fd9e4467678ac24c6bcc7c2664095f8d
ranieresilva <ranieresilva@baskarasoftware.com>
2022-10-19 07:34:22 -0300

    ajuste na configuração com o mongodb

--- api/database.js
+++ api/database.js
@@ -6,12 +6,17 @@ require('./models/Specie')
 require('./models/Product')
 require('./config/passport')
 
+const fs = require('fs')
+
 const mongoose = require('mongoose')
 mongoose.set('useCreateIndex', true)
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 if (isProduction) {
+  // Create certificate file from env var (because mongoose doesnt accept cert data directly)
+  fs.writeFileSync('./api/config/mongodb.ca.crt', process.env.MONGO_CA_CERT)
+
   mongoose.connect(process.env.MONGODB_URI, {
     keepAlive: 1,
     connectTimeoutMS: 30000,
@@ -19,11 +24,14 @@ if (isProduction) {
     reconnectInterval: 1000,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
 } else {
-  mongoose.connect('mongodb://localhost/cadeia-produtiva', {
+  mongoose.connect('mongodb://localhost/' + process.env.npm_package_name, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
   })
   mongoose.set('debug', true)
 }
