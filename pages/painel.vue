ebe12b253f7f686a908ce384fde41898ef924a5c
lucas dias <lucashsd99@hotmail.com>
2023-02-03 05:45:00 -0300

    tela com os dados publicados

--- pages/painel.vue
+++ pages/painel.vue
@@ -1,27 +1,83 @@
 <template>
-  <div>
-    <breadcrumb active="Minha conta" />
-    <b-container fluid class="mb-4">
-      <b-card-group deck>
-        <b-card no-body>
-          <b-card-header>
-            <i class="fas fa-tools" /> Operacional
-          </b-card-header>
-          <b-card-body>
-            <n-link class="btn btn-secondary mr-1 mb-1" to="/estoque">
-              Estoque
-            </n-link>
-          </b-card-body>
-        </b-card>
-      </b-card-group>
-    </b-container>
+  <div class="SpeciesProduct">
+    <breadcrumb />
+    <div class="panel">
+      <div class="panel-body">
+        <div class="info-content">
+          <div class="row">
+            <div class="text-right col-sm-8">
+              <input
+                v-model="filters.search"
+                type="search"
+                :placeholder="'Buscar'"
+                class="form-control search-input"
+              />
+            </div>
+          </div>
+          <br />
+          <no-item :list="priceInformations" />
+          <b-table
+            class="table b-table b-table-stacked-md table-striped"
+            :fields="table_fields"
+            :items="priceInformations"
+            :sort-by="'name'"
+            :filter="filters.search"
+          >
+            <template #cell(local)="data">
+              {{ data.item.city }}
+              /
+              {{ data.item.uf }}
+            </template>
+            <template #cell(createdAt)="data">
+              {{ data.item.createdAt | moment('DD/MM/YYYY') }}
+            </template>
+            <template #cell(product)="data">
+              {{ data.item.product.code }}
+            </template>
+          </b-table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
 import Breadcrumb from '@/components/Breadcrumb'
 export default {
   components: {
     Breadcrumb,
+  },
+  data() {
+    return {
+      filters: { search: null },
+      table_fields: [
+        {
+          key: 'local',
+          label: 'cidade/estado',
+          sortable: true,
+        },
+        {
+          key: 'createdAt',
+          label: 'Data',
+          sortable: true,
+        },
+        {
+          key: 'product',
+          label: 'Produto',
+          sortable: true,
+        },
+      ],
+      priceInformations: [],
+    }
+  },
+
+  async created() {
+    await this.list()
+  },
+  methods: {
+    async list() {
+      this.priceInformations = await this.$axios.$get('priceInformations')
+    },
   },
 }
 </script>
