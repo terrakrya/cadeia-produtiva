b94139b8bf201554a85c043b15b53a5d4042247c
lucas dias <lucashsd99@hotmail.com>
2023-02-17 15:14:34 -0300

    Popup com a tabela de equivalência

--- pages/operacional/informacao-preco/index.vue
+++ pages/operacional/informacao-preco/index.vue
@@ -14,6 +14,73 @@
             >
               <b-icon-plus /> {{ 'Registrar' }}
             </n-link>
+            <div>
+              <b-button
+                id="show-btn"
+                class="btn btn-primary"
+                variant="danger"
+                @click="$bvModal.show('bv-modal')"
+                >Tradutor de medidas</b-button
+              >
+
+              <b-modal id="bv-modal" hide-footer>
+                <template #modal-title> Conversões</template>
+                <table class="table b-table b-table-stacked-md table-striped">
+                  <thead>
+                    <tr>
+                      <th>Unidade</th>
+                      <!--<th>Lata</th>
+                      <th>Litros</th>-->
+                      <th>Kg</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <td>Lata/Latão</td>
+                    <!--<td>1</td>
+                    <td>20</td>-->
+                    <td>12</td>
+                  </tbody>
+                  <tbody>
+                    <td>Caixa</td>
+                    <!--<td>2</td>
+                    <td>40</td>-->
+                    <td>24</td>
+                  </tbody>
+                  <tbody>
+                    <td>Hectolitro</td>
+                    <!--<td>5</td>
+                    <td>100</td>-->
+                    <td>60</td>
+                  </tbody>
+                  <tbody>
+                    <td>Saca</td>
+                    <!--<td>5</td>
+                    <td>100</td>-->
+                    <td>60</td>
+                  </tbody>
+                  <tbody>
+                    <td>Barrica</td>
+                    <!--<td>6</td>
+                    <td>120</td>-->
+                    <td>72</td>
+                  </tbody>
+                  <tbody>
+                    <td>Tonelada</td>
+                    <!--<td></td>
+                    <td></td>-->
+                    <td>1000</td>
+                  </tbody>
+                </table>
+                <b-button
+                  class="btn btn-primary"
+                  variant="danger"
+                  block
+                  @click="$bvModal.hide('bv-modal')"
+                  >Fechar</b-button
+                >
+              </b-modal>
+            </div>
           </div>
         </div>
         <div class="info-content">
@@ -83,6 +150,7 @@ export default {
   },
   data() {
     return {
+      translator: true,
       filters: { search: null },
       table_fields: [
         {
