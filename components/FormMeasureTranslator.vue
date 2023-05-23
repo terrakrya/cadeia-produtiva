6bb0a738ab46e6a22c608eedd579a2a10eb77f3f
lucassd99 <lucashsd99@hotmail.com>
2023-05-23 16:22:58 -0300

    popup de praça

---
+++ components/FormMeasureTranslator.vue
@@ -0,0 +1,67 @@
+<template>
+    <b-modal :id="id" hide-footer>
+        <template #modal-title> Conversões</template>
+            <table class="table b-table b-table-stacked-md table-striped">
+                <thead>
+                    <tr>
+                      <th>Unidade</th>
+                      <!--<th>Lata</th>
+                      <th>Litros</th>-->
+                      <th>Kg</th>
+                      <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td>Lata/Latão</td>
+                    <!--<td>1</td>
+                    <td>20</td>-->
+                    <td>12</td>
+                </tbody>
+                <tbody>
+                    <td>Caixa</td>
+                    <!--<td>2</td>
+                    <td>40</td>-->
+                    <td>24</td>
+                </tbody>
+                <tbody>
+                    <td>Hectolitro</td>
+                    <!--<td>5</td>
+                    <td>100</td>-->
+                    <td>60</td>
+                </tbody>
+                <tbody>
+                    <td>Saca</td>
+                    <!--<td>5</td>
+                    <td>100</td>-->
+                    <td>60</td>
+                </tbody>
+                <tbody>
+                    <td>Barrica</td>
+                    <!--<td>6</td>
+                    <td>120</td>-->
+                    <td>72</td>
+                </tbody>
+                <tbody>
+                    <td>Tonelada</td>
+                    <!--<td></td>
+                    <td></td>-->
+                    <td>1000</td>
+                </tbody>
+            </table>
+            <b-button
+                class="btn btn-primary"
+                variant="danger"
+                 block
+                @click="$bvModal.hide(id)"
+                >Fechar</b-button
+            >
+    </b-modal>
+</template>
+<script>
+export default {
+    name: 'FormMeasureTranslator',
+    props: {
+     id: '',
+    },
+}
+</script>
---
+++ components/FormSquareTranslator.vue
@@ -0,0 +1,50 @@
+<template>
+    <b-modal :id="id">
+        <template>
+    <b-table
+    class="table b-table b-table-stacked-md table-striped"
+    :fields="table_fields"
+    :items="pracas"
+  >
+    <template #cell(uf)="data">
+      {{ data.item.estado }}
+    </template>
+    <template #cell(city)="data">
+      {{ data.item.cidade }}
+    </template>
+    <template #cell(square)="data">
+      {{ data.item.nome }}
+    </template>
+  </b-table>
+</template>
+</b-modal>
+</template>
+<script>
+import pracas from '@/data/praca.json'
+export default {
+  name: 'FormSquareTranslator',
+  props: {
+    id: '',
+  },
+  data() {
+    return {
+      pracas,
+      table_fields: [
+        {
+          key: 'uf',
+          label: 'Estado',
+        },
+        {
+          key: 'city',
+          label: 'cidade',
+        },
+        {
+          key: 'square',
+          label: 'Praça',
+        },
+      ],
+    }
+  },
+}
+</script>
+
