1cae7b60fb944f7db9768cb0dc11a14bd99940f7
lucassd99 <lucashsd99@hotmail.com>
2023-06-29 17:26:19 -0300

    ajuste popup de praça

--- components/FormSquareTranslator.vue
+++ components/FormSquareTranslator.vue
@@ -1,23 +1,35 @@
 <template>
-    <b-modal :id="id">
-        <template>
-    <b-table
-    class="table b-table b-table-stacked-md table-striped"
-    :fields="table_fields"
-    :items="pracas"
-  >
-    <template #cell(uf)="data">
-      {{ data.item.estado }}
-    </template>
-    <template #cell(city)="data">
-      {{ data.item.cidade }}
-    </template>
-    <template #cell(square)="data">
-      {{ data.item.nome }}
-    </template>
-  </b-table>
+  <b-modal :id="id">
+    <template >
+  <div>
+    <table v-for="(a, index) in groupedlist" :key="index" class="table">
+      <tr>
+        <td>
+          <strong>{{ index  }}</strong>
+        </td>
+      </tr>
+      <tr>
+        <thead>
+          <td>
+            <strong>UF</strong>
+          </td>
+          <td>
+            <strong>Município</strong>
+          </td>
+        </thead>
+        <tbody v-for="(dados, index2) in a" :key="index2">
+          <td style="width: 30%">
+            <small>{{ dados.estado }}</small>
+          </td>
+          <td style="width: 70%">
+            <small>{{ dados.cidade }}</small>
+          </td>
+        </tbody>
+      </tr>
+    </table>
+  </div>
 </template>
-</b-modal>
+  </b-modal>
 </template>
 <script>
 import pracas from '@/data/praca.json'
@@ -29,22 +41,39 @@ export default {
   data() {
     return {
       pracas,
-      table_fields: [
-        {
-          key: 'uf',
-          label: 'Estado',
-        },
-        {
-          key: 'city',
-          label: 'Município',
-        },
-        {
-          key: 'square',
-          label: 'Praça',
-        },
-      ],
+      groupedlist:[],
     }
   },
+  created (){
+    this.list()
+  },
+  methods: {
+    list(){
+      function groupBy(arr, property) {
+        return arr.reduce(function (memo, x) {
+          if (!memo[x[property]]) { memo[x[property]] = []; }
+          memo[x[property]].push(x);
+          return memo;
+        },   {});
+      };
+      const groupedlist = groupBy(pracas,'nome')
+      this.groupedlist = groupedlist 
+    }
+    
+  }
 }
 </script>
+<style>
+.table
+  table,td,thead {
+    text-align: center;
+    width: 300px;
+    border-collapse: collapse;
+  }
 
+  th,td {
+    background-color: #fff;
+    border: 1px solid #ddd;
+    padding: 5px;
+  }
+</style>
