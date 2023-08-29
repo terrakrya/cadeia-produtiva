65d2b96cf25d075d81e6a751a40a272e94fab815
Diego M. Rodrigues <diegomr86@gmail.com>
2023-08-29 20:06:36 -0300

    Aplica melhorias visuais e de usabilidade no menu

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,58 +1,66 @@
-import { isAdmin } from '~/api/config/auth';
-
 <template>
   <div>
     <b-list-group class="sidebar color-white">
-      <b-list-group-item v-if="isAdmin || isGlobalManager || isManager" class="color-white">
-        Cadastros
-        <b-icon-chevron-down />
-      </b-list-group-item>
-      <b-list-group-item v-if="isAdmin || isGlobalManager || isManager" class="bg-brown-2">
-        <b-nav v-if="isAdmin || isGlobalManager" vertical>
-          <b-nav-item to="/cadastros/especies">Espécies</b-nav-item>
+      <b-list-group-item v-if="isAdmin || isGlobalManager || isManager">
+        <b-nav v-if="isAdmin || isGlobalManager">
+          <b-nav-item class="text-white" to="/cadastros/especies"
+            >Espécies</b-nav-item
+          >
         </b-nav>
-        <b-nav v-if="isAdmin || isGlobalManager" vertical>
-          <b-nav-item to="/cadastros/especies-produtos">
+        <b-nav v-if="isAdmin || isGlobalManager">
+          <b-nav-item class="text-white" to="/cadastros/especies-produtos">
             Classificações de espécies/produtos
           </b-nav-item>
         </b-nav>
-        <b-nav v-if="isAdmin || isGlobalManager" vertical>
-          <b-nav-item to="/cadastros/tipos">
+        <b-nav v-if="isAdmin || isGlobalManager">
+          <b-nav-item class="text-white" to="/cadastros/tipos">
             Tipos de boas práticas e certificação
           </b-nav-item>
         </b-nav>
-        <b-nav v-if="isAdmin || isGlobalManager" vertical>
-          <b-nav-item to="/cadastros/produtos">Produtos</b-nav-item>
+        <b-nav v-if="isAdmin || isGlobalManager">
+          <b-nav-item class="text-white" to="/cadastros/produtos"
+            >Produtos</b-nav-item
+          >
         </b-nav>
-        <b-nav v-if="isAdmin || isGlobalManager || isManager" vertical>
-          <b-nav-item to="/cadastros/organizacoes">Organizações</b-nav-item>
+        <b-nav v-if="isAdmin || isGlobalManager || isManager">
+          <b-nav-item class="text-white" to="/cadastros/organizacoes"
+            >Organizações</b-nav-item
+          >
         </b-nav>
-        <b-nav v-if="isAdmin || isGlobalManager || isManager" vertical>
-          <b-nav-item to="/cadastros/usuarios">Usuários</b-nav-item>
+        <b-nav v-if="isAdmin || isGlobalManager || isManager">
+          <b-nav-item class="text-white" to="/cadastros/usuarios"
+            >Usuários</b-nav-item
+          >
         </b-nav>
-        <b-nav v-if="isAdmin || isGlobalManager || isManager" vertical>
-          <b-nav-item to="/cadastros/areas-geograficas">Áreas geográficas</b-nav-item>
+        <b-nav v-if="isAdmin || isGlobalManager || isManager">
+          <b-nav-item class="text-white" to="/cadastros/areas-geograficas"
+            >Áreas geográficas</b-nav-item
+          >
         </b-nav>
       </b-list-group-item>
       <b-list-group-item>
-        <i class="fas fa-tools" />
-        Operacional
-        <b-icon-chevron-down />
-      </b-list-group-item>
-      <b-list-group-item class="bg-brown-2">
-        <b-nav vertical>
-          <b-nav-item to="/operacional/informacao-preco">
+        <b-nav>
+          <b-nav-item class="text-white" to="/operacional/informacao-preco">
             Coleta de preços
           </b-nav-item>
         </b-nav>
       </b-list-group-item>
       <b-list-group-item>
-        <b-icon-person /> {{ currentUser.nickname || currentUser.name }}
-        <b-icon-chevron-down />
+        <b-nav>
+          <b-nav-item
+            class="text-white"
+            :to="'/cadastros/usuarios/' + currentUser._id + '/perfil'"
+          >
+            Minha conta
+          </b-nav-item>
+        </b-nav>
       </b-list-group-item>
-      <b-list-group-item class="bg-brown-2">
-        <b-nav vertical>
-          <b-nav-item @click="$auth.logout()">Sair</b-nav-item>
+
+      <b-list-group-item>
+        <b-nav>
+          <b-nav-item class="text-white" @click="$auth.logout()"
+            >Sair</b-nav-item
+          >
         </b-nav>
       </b-list-group-item>
     </b-list-group>
