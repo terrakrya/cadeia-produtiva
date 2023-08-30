31a5645e14ae4dea03fba4733c304ff721c012f6
Diego M. Rodrigues <diegomr86@gmail.com>
2023-08-29 21:14:58 -0300

    Corrige erro de alinhamento das tabelas

--- components/FormSquareTranslator.vue
+++ components/FormSquareTranslator.vue
@@ -1,34 +1,34 @@
 <template>
   <b-modal :id="id">
-    <template >
-  <div>
-    <table v-for="(a, index) in groupedlist" :key="index" class="table">
-      <tr>
-        <td>
-          <strong>{{ index  }}</strong>
-        </td>
-      </tr>
-      <tr>
-        <thead>
-          <td>
-            <strong>UF</strong>
-          </td>
-          <td>
-            <strong>Município</strong>
-          </td>
-        </thead>
-        <tbody v-for="(dados, index2) in a" :key="index2">
-          <td style="width: 30%">
-            <small>{{ dados.estado }}</small>
-          </td>
-          <td style="width: 70%">
-            <small>{{ dados.cidade }}</small>
-          </td>
-        </tbody>
-      </tr>
-    </table>
-  </div>
-</template>
+    <template>
+      <div class="square-translator">
+        <table v-for="(a, index) in groupedlist" :key="index" class="table">
+          <tr>
+            <td>
+              <strong>{{ index }}</strong>
+            </td>
+          </tr>
+          <tr>
+            <thead>
+              <td>
+                <strong>UF</strong>
+              </td>
+              <td>
+                <strong>Município</strong>
+              </td>
+            </thead>
+            <tbody v-for="(dados, index2) in a" :key="index2">
+              <td style="width: 30%">
+                <small>{{ dados.estado }}</small>
+              </td>
+              <td style="width: 70%">
+                <small>{{ dados.cidade }}</small>
+              </td>
+            </tbody>
+          </tr>
+        </table>
+      </div>
+    </template>
   </b-modal>
 </template>
 <script>
@@ -36,44 +36,52 @@ import pracas from '@/data/praca.json'
 export default {
   name: 'FormSquareTranslator',
   props: {
-    id: '',
+    id: {
+      type: String,
+      required: true,
+    },
   },
   data() {
     return {
       pracas,
-      groupedlist:[],
+      groupedlist: [],
     }
   },
-  created (){
+  created() {
     this.list()
   },
   methods: {
-    list(){
+    list() {
       function groupBy(arr, property) {
         return arr.reduce(function (memo, x) {
-          if (!memo[x[property]]) { memo[x[property]] = []; }
-          memo[x[property]].push(x);
-          return memo;
-        },   {});
-      };
-      const groupedlist = groupBy(pracas,'nome')
-      this.groupedlist = groupedlist 
-    }
-    
-  }
+          if (!memo[x[property]]) {
+            memo[x[property]] = []
+          }
+          memo[x[property]].push(x)
+          return memo
+        }, {})
+      }
+      const groupedlist = groupBy(pracas, 'nome')
+      this.groupedlist = groupedlist
+    },
+  },
 }
 </script>
 <style>
-.table
-  table,td,thead {
+.square-translator {
+  .table table,
+  td,
+  thead {
     text-align: center;
     width: 300px;
     border-collapse: collapse;
   }
 
-  th,td {
+  .square-translator th,
+  td {
     background-color: #fff;
     border: 1px solid #ddd;
-    padding: 5px;
+    padding: 5px;
   }
+}
 </style>
