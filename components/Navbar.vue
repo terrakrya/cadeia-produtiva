08903a983a2ee0d59377ea3c594e934d5e92b648
Henrique Weiss <henriqueweiss@hotmail.com>
2024-01-31 14:04:46 -0300

    Adequação de navegação via botões e de estilo

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,11 +6,12 @@
       </b-navbar-toggle>
       <b-navbar-brand class="text-white ml-2" to="/painel">IÊ</b-navbar-brand>
     </div>
-    <div>
-  <button class="btn-user">
-    <div class="btn-user-text">Olá, {{ currentUser.name.split(' ')[0] }}</div>
-  </button>
-</div>
+    <b-link
+      :to="`/cadastros/usuarios/${currentUser._id}/perfil`"
+      class="btn-user"
+    >
+      <div class="btn-user-text">Olá, {{ currentUser.name.split(' ')[0] }}</div>
+    </b-link>
 
     <b-collapse id="sidebar" is-nav>
       <div>
