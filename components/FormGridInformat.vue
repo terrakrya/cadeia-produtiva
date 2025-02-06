f1992d53e5166bb9c82b7e321b330d8ecbd379c2
Henrique <henriqueweiss@hotmail.com>
2025-02-06 18:37:31 -0300

    Texto mostrando quantos registros aparecem na tabela de Dados Publicados

--- components/FormGridInformat.vue
+++ components/FormGridInformat.vue
@@ -1,41 +1,48 @@
 <template>
-  <b-table
-    v-if="list && list.length"
-    class="table b-table b-table-stacked-md table-striped"
-    :fields="table_fields"
-    :items="list"
-    :sort-by.sync="sortBy"
-    :sort-desc.sync="sortDesc"
-    stacked="lg"
-  >
-    <template #cell(from)="data">
-      {{ data.item.buyerFrom }}
-    </template>
-    <template #cell(to)="data">
-      {{ data.item.buyerTo }}
-    </template>
-    <template #cell(messenger)="data">
-      {{ data.item.messenger }}
-    </template>
-    <template #cell(region)="data">
-      {{ data.item.region || 'Não informado' }}
-    </template>
-    <template #cell(city)="data">
-      {{ data.item.city }}
-    </template>
-    <template #cell(date)="data">
-      {{ data.item.date }}
-    </template>
-    <template #cell(averagePrice)="data">
-      {{ convertPrice(data.item.averagePrice) | moeda }}
-    </template>
-    <template #cell(minimumPrice)="data">
-      {{ convertPrice(data.item.minimumPrice) | moeda }}
-    </template>
-    <template #cell(maximumPrice)="data">
-      {{ convertPrice(data.item.maximumPrice) | moeda }}
-    </template>
-  </b-table>
+  <div v-if="list && list.length">
+    <div class="d-flex justify-content-end mb-2">
+      <small class="text-muted">Exibindo {{ list.length }} registro(s)</small>
+    </div>
+    <b-table
+      class="table b-table b-table-stacked-md table-striped"
+      :fields="table_fields"
+      :items="list"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      stacked="lg"
+    >
+      <template #cell(from)="data">
+        {{ data.item.buyerFrom }}
+      </template>
+      <template #cell(to)="data">
+        {{ data.item.buyerTo }}
+      </template>
+      <template #cell(messenger)="data">
+        {{ data.item.messenger }}
+      </template>
+      <template #cell(region)="data">
+        {{ data.item.region || 'Não informado' }}
+      </template>
+      <template #cell(city)="data">
+        {{ data.item.city }}
+      </template>
+      <template #cell(date)="data">
+        {{ data.item.date }}
+      </template>
+      <template #cell(averagePrice)="data">
+        {{ convertPrice(data.item.averagePrice) | moeda }}
+      </template>
+      <template #cell(minimumPrice)="data">
+        {{ convertPrice(data.item.minimumPrice) | moeda }}
+      </template>
+      <template #cell(maximumPrice)="data">
+        {{ convertPrice(data.item.maximumPrice) | moeda }}
+      </template>
+    </b-table>
+  </div>
+  <div v-else>
+    <p class="text-center text-muted">Nenhum registro encontrado.</p>
+  </div>
 </template>
 <script>
 export default {
