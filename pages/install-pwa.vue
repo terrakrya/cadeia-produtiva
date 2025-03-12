c933e7b189e918ef608a750228e09498036dc15d
Henrique <henriqueweiss@hotmail.com>
2025-03-12 15:31:08 -0300

    Melhoria no texto de instalação do PWA para iOS

--- pages/install-pwa.vue
+++ pages/install-pwa.vue
@@ -10,11 +10,17 @@
           <b-card class="bg-brown-1">
             <b-card-body>
               <h2>Instale nosso aplicativo</h2>
-              <p>Para uma melhor experiência, instale o aplicativo em seu dispositivo:</p>
-              
+              <p>
+                Para uma melhor experiência, instale o aplicativo em seu
+                dispositivo:
+              </p>
+
               <!-- Install button - only shows when prompt is available -->
               <div v-if="deferredPrompt" class="mt-4">
-                <b-button class="btn btn-white btn-lg btn-block" @click="installPWA">
+                <b-button
+                  class="btn btn-white btn-lg btn-block"
+                  @click="installPWA"
+                >
                   Instalar Aplicativo
                 </b-button>
               </div>
@@ -24,25 +30,30 @@
                 <h4>Como instalar:</h4>
                 <ul class="text-left">
                   <li v-if="isIOS" class="mb-2">
-                    Abra o menu do navegador 
-                    <span class="text-primary">
-                      <font-awesome-icon icon="fa-solid fa-share-from-square" />
-                    </span> 
-                    e selecione "Adicionar à Tela de Início"
+                    Toque no ícone de compartilhamento do navegador que fica no
+                    centro na parte de baixo da tela
+                    <span class="text-white">
+                      <font-awesome-icon
+                        icon="fa-solid fa-arrow-up-from-bracket"
+                      />
+                    </span>
+                    e, em seguida, selecione "Adicionar à Tela de Início".
                   </li>
                   <li v-if="isAndroid" class="mb-2">
                     No navegador, abra o menu (⋮ ou ≡) no canto da tela
                     <span class="text-primary">
                       <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" />
-                    </span> 
-                    e selecione "Adicionar à tela inicial" ou "Instalar aplicativo"
+                    </span>
+                    e selecione "Adicionar à tela inicial" ou "Instalar
+                    aplicativo"
                   </li>
                   <li v-if="!isIOS && !isAndroid" class="mb-2">
-                    No seu navegador, procure pelo ícone de instalação 
+                    No seu navegador, procure pelo ícone de instalação
                     <span class="text-primary">
                       <font-awesome-icon icon="fa-solid fa-download" />
-                    </span> 
-                    na barra de endereço ou no menu do navegador e selecione "Instalar" ou "Adicionar à tela inicial"
+                    </span>
+                    na barra de endereço ou no menu do navegador e selecione
+                    "Instalar" ou "Adicionar à tela inicial"
                   </li>
                 </ul>
               </div>
@@ -57,12 +68,12 @@
 <script>
 export default {
   layout: 'front',
-  
+
   data() {
     return {
       isIOS: process.client && /iPad|iPhone|iPod/.test(navigator.userAgent),
       isAndroid: process.client && /Android/.test(navigator.userAgent),
-      deferredPrompt: null
+      deferredPrompt: null,
     }
   },
 
@@ -95,14 +106,13 @@ export default {
 
       // Wait for the user to respond to the prompt
       const { outcome } = await this.deferredPrompt.userChoice
-      
+
       // Optionally log the outcome
       console.log(`User response to the install prompt: ${outcome}`)
 
       // Clear the deferredPrompt - it can't be used again
       this.deferredPrompt = null
     },
-  }
+  },
 }
 </script>
-  
